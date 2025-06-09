0c62579518040394e86cf0b4b2baec140d6fa75b
smartaistack <contact@smartaistack.org>
2025-06-09 17:30:25 +0200

    Update test_import.py

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,8 +1,8 @@
 import sys
 import os
 
-# Legg til path slik at 'core' kan importeres
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+# Legg til repo-roten i sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from core.test_trigger import test_trigger  # noqa: F401
 
