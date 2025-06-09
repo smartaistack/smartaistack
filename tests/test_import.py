f8b1eb6cda3d626c581e84ef2d02ec902a2379ad
smartaistack <contact@smartaistack.org>
2025-06-09 17:34:48 +0200

    Update test_import.py

--- tests/test_import.py
+++ tests/test_import.py
@@ -4,7 +4,7 @@ import os
 # Legg til repo-roten i sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from core.test_trigger import test_trigger  # noqa: F401
+from core.core.test_trigger import test_trigger
 
 def test_import():
     assert True
