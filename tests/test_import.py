12e68ec6e29320c741e78e5badca7246e46529c3
smartaistack <contact@smartaistack.org>
2025-06-09 16:52:04 +0200

    Update test_import.py

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,8 +1,10 @@
 import sys
 import os
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from core import test_trigger  # noqa: F401
+
 
 def test_import():
     assert True
