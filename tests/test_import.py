98587fcf95e3ae3e144ccb16c92fc1b4208c47ea
smartaistack <contact@smartaistack.org>
2025-06-09 17:04:31 +0200

    Update test_import.py

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,7 +1,9 @@
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+# Gå opp to nivåer slik at 'core' blir tilgjengelig
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
 from core.test_trigger import test_trigger  # noqa: F401
 
 def test_import():
